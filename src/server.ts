c799146668a00622ac8f34ac1e79182422553a00
Leo <leandroano8@gmail.com>
2024-05-18 20:23:41 -0300

    adicionada a mesangem de erro na criação do filme

--- src/server.ts
+++ src/server.ts
@@ -6,6 +6,7 @@ const port = 3000;
 const app = express();
 const prisma = new PrismaClient;
 
+app.use(express.json())
 
 app.get("/movies", async (_, res) => {
   const movies = await prisma.movie.findMany({
@@ -19,6 +20,29 @@ app.get("/movies", async (_, res) => {
   });
   res.json(movies)
 })
+
+app.post("/movies", async (req, res) => {
+
+  const {title, genre_id, language_id, oscar_count, release_date} = req.body
+
+try { 
+  await prisma.movie.create({
+    data: {
+      title,
+      genre_id,
+      language_id,
+      oscar_count,
+      release_date: new Date(release_date)
+    }
+  })
+} catch(err) {
+  return res.status(500).send({mesangem: "Falha ao cadastra um filme"})
+}
+
+
+  res.status(201).send()
+})
+
 
 app.listen(port, () => {
   console.log('Servidor em exercução em http://localhost:3000/')
